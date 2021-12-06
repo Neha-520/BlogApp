d194dee087f96a394b4b1d02cb9c001b86c5f3bc
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-12-07 01:51:19 +0530

    login+register

--- blogapp/src/App.js
+++ blogapp/src/App.js
@@ -1,18 +1,37 @@
+import { Switch, Route } from 'react-router-dom';
 import { Home } from "./Pages/Home/Home";
 import { NavBar } from "./components/Navbar/NavBar";
 import { Single } from "./Pages/single/Single";
 import { Write } from "./Pages/write/Write";
 import { Setting } from "./Pages/settings/Setting";
+import { Login } from "./Pages/login/Login";
+import { Register } from "./Pages/register/Register";
+
 
 
 function App() {
   return (
     <>
       <NavBar />
-      {/* <Home /> */}
-      {/* <Single /> */}
-      {/* <Write /> */}
-      <Setting />
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/posts">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login"> <Login /></Route>
+        <Route path="/post/:id">
+          <Single />
+        </Route>
+        <Route path="/write"><Write /></Route>
+        <Route path="/settings">
+          <Setting />
+        </Route>
+      </Switch>
     </>
   );
 }
