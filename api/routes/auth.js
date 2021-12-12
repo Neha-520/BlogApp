0e27f1de3b62fc623d0817d43a8a5512dba17c37
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-12-12 16:43:37 +0530

    login + registeter auth

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,10 +1,22 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require('bcrypt');
 
 //REGISTER
 router.post("/register", async (req, res) => {
     try {
-        const newUser = new User();
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+        const newUser = new User({
+            userName: req.body.userName,
+            email: req.body.email,
+            password: hashedPass,
+        });
+
+        const user = await newUser.save(); //save method from mongoose
+        res.status(200).json(user);
     } catch (err) {
         res.status(500).json(err);
     }
@@ -13,3 +25,21 @@ router.post("/register", async (req, res) => {
 
 
 //LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ userName: req.body.userName })
+        if (!user) res.status(400).json("Wrong cresentials!");
+
+        const validate = await bcrypt.compare(req.body.password, user.password);
+        if (!validate) res.status(400).json("Wrong cresentials!");
+
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+
+module.exports = router;
