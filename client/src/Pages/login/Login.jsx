f2479fad099c2a6e9ed8dc42f485de8fc66d34b9
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-12-15 16:47:31 +0530

    login states

--- client/src/Pages/login/Login.jsx
+++ client/src/Pages/login/Login.jsx
@@ -1,16 +1,39 @@
+import axios from 'axios';
+import { useContext, useRef } from 'react'
 import { Link } from 'react-router-dom'
+import { Context } from '../../context/Context';
 import './login.css'
 
 export const Login = () => {
+
+    const userRef = useRef();
+    const passwordRef = useRef();
+    const { user, dispatch, isFetching } = useContext(Context)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        dispatch({ type: "LOGIN_START" });
+        try {
+            const res = await axios.post("/auth/login", {
+                userName: userRef.current.value,
+                password: passwordRef.current.value
+            })
+            dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+        } catch (err) {
+            dispatch({ type: "LOGIN_FAILURE" });
+            console.log(err);
+        }
+    }
+    console.log(user);
     return (
         <div className="login">
             <span className="loginTitle">Login</span>
-            <form className="loginForm">
+            <form className="loginForm" onSubmit={handleSubmit}>
                 <label>Username</label>
-                <input className="loginInput" type="text" placeholder="Enter your username" />
+                <input ref={userRef} className="loginInput" type="text" placeholder="Enter your username" />
                 <label>Password</label>
-                <input className="loginInput" type="password" placeholder="Enter your password..." />
-                <button className="loginButton">Login</button>
+                <input ref={passwordRef} className="loginInput" type="password" placeholder="Enter your password..." />
+                <button className="loginButton" type='submit'>Login</button>
             </form>
             <button className="loginRegisterButton">
                 <Link className="link" to="/register">
